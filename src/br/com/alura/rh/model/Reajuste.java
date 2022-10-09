cd6774e487d1c94332a606d4b75d6222e73c9c71
thiagomarqs <thiagodsmarques@hotmail.com>
2022-10-09 17:47:27 -0300

    Tipos de reajuste: promoção e anuênio

---
+++ src/br/com/alura/rh/model/Reajuste.java
@@ -0,0 +1,10 @@
+package br.com.alura.rh.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public interface Reajuste {
+
+    public BigDecimal valor();
+    public LocalDate data();
+}
